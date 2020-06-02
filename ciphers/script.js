8147a191e9116058187c951653fd8d4d32e063f6
Marri Stephen <stephenmarri7@gmail.com>
2020-06-02 14:34:42 +0530

    added copy to clipboard

--- ciphers/script.js
+++ ciphers/script.js
@@ -4,15 +4,17 @@ const encodeShift = document.querySelector('#encode__number')
 const decodeUnShift = document.querySelector('#decode__number')
 const encodeButton = document.querySelector('#encode__button')
 const decodeButton = document.querySelector('#decode__button')
+const copyButton = document.querySelector('#copy-button');
 
 encodeButton.addEventListener('click',encodeHandler);
 decodeButton.addEventListener('click',decodeHandler);
+copyButton.addEventListener('click',copyHandler)
 
 function encodeHandler(){
     let text = userInput.value;
     let shift = parseInt(encodeShift.value)
 
-    if(text.length > 0 && shift>0){
+    if(text.length > 0 && shift>=0){
         writeOuput(encode(text,shift));
     }
 }
@@ -21,11 +23,14 @@ function decodeHandler(){
     let text = userInput.value;
     let shift = parseInt(decodeUnShift.value)
 
-    if(text.length > 0 && shift>0){
+    if(text.length > 0 && shift>=0){
         writeOuput(decode(text,shift));
     }
 }
 
+function copyHandler(){
+    navigator.clipboard.writeText(output.textContent)
+}
 
 function writeOuput(str){
     output.textContent=str;
